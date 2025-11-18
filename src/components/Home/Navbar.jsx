91f8438cf6fb4d53785a20c448fe7a7d3644fddc
DubanPrograDVR <156736859+DubanPrograDVR@users.noreply.github.com>
2025-11-17 21:59:07 -0300

    first commit

---
+++ src/components/Home/Navbar.jsx
@@ -0,0 +1,27 @@
+import "./styles/";
+
+export default function Navbar() {
+  return (
+    <>
+      <header className="cabeza">
+        <div className="div-logo">
+          <img src="" alt="" />
+        </div>
+        <nav className="navbar">
+          <li>
+            <a href="">Panoramas</a>
+          </li>
+          <li>
+            <a href="">Superguía Extrovertidos</a>
+          </li>
+          <li>
+            <a href="">Publicar Panorama</a>
+          </li>
+          <li>
+            <a href="">Publicar Publicar Negocio</a>
+          </li>
+        </nav>
+      </header>
+    </>
+  );
+}
